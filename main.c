efad4412f93cd7275cf6acde7cfb7536b7690d45
ShawnzWal <darshanwalchale@gmail.com>
2020-06-10 12:36:05 +0000

    added time.h, changed variables to be time_t, changed char b_book_status to bool b_available

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <bool.h>
 #include <ctype.h>
+#include <time.h>
 
 // DEFINES
 
@@ -12,14 +13,14 @@
 
 //Structure variables starting with single characters will be used in different functions
 typedef struct{
-    char b_book_title[30];
+    char b_book_title[40];
     char b_book_author[30];
     int b_book_ID; //10 digit ID
     int b_issue_ID; //10 digit ID
-    int book_date_of_arrival; //DDMMYYYY format
+    time_t book_date_of_arrival;    //
     int b_user_ID; //10 digit ID
-    int b_date_issue; //DDMMYYYY format
-    char b_book_status; //'A' for available, 'R': Reserved, 'I': Issued
+    time_t b_date_issue;            //
+    bool b_available;               //true means available
 } BOOK;
 
 typedef struct{
@@ -27,7 +28,7 @@ typedef struct{
     char user_name; //
     int u_book_ID; //10 digit ID
     int u_issue_ID; //10 digit ID
-    int u_date_issue; //DDMMYYYY format
+    time_t u_date_issue;    //
     char u_user_pwd[30]; //User password
     bool admin;
 } USER;
