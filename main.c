5069fe260649cc6acb657378b580e2d8613af9c0
ShreyasSR <sr.shreyu2001@gmail.com>
2020-06-28 07:27:21 +0000

    Small changes in search

--- main.c
+++ main.c
@@ -1391,6 +1391,8 @@ void searchBookbyTitle(BOOKNODE *head)
                 sprintf(curr_ID, "%ld", current->book.b_book_ID); //Appending ID to display
                 strcat(closest_search[cs_count],"\n\tID: ");
                 strcat(closest_search[cs_count],curr_ID);
+                strcat(closest_search[cs_count],"\n\tAuthor: ");
+                 strcat(closest_search[cs_count],current->book.b_book_author);
                 strcat(closest_search[cs_count],"\n\tStatus: ");
                     if(current->book.b_book_status=='A')
                         strcat(closest_search[cs_count], "Available ");
@@ -1400,6 +1402,7 @@ void searchBookbyTitle(BOOKNODE *head)
                         strcat(closest_search[cs_count], "Not available (Issued)");
                     else
                         strcat(closest_search[cs_count], "Undefined ");
+
                  strcat(closest_search[cs_count],"\n");
                 cs_count++;
             }
@@ -1417,6 +1420,8 @@ void searchBookbyTitle(BOOKNODE *head)
                 sprintf(curr_ID, "%ld", current->book.b_book_ID); //Appending ID to display
                 strcat(same_title_found[title_found],"\n\tID: ");
                 strcat(same_title_found[title_found],curr_ID);
+                strcat(same_title_found[title_found],"\n\tAuthor: ");
+                strcat(same_title_found[title_found],current->book.b_book_author);
                 strcat(same_title_found[title_found],"\n\tStatus: ");
                     if(current->book.b_book_status=='A')
                         strcat(same_title_found[title_found], "Available ");
@@ -1458,19 +1463,9 @@ void searchBookbyTitle(BOOKNODE *head)
         }
         //To print all books found of same title (if any)
         if(title_found>0){
-        /*
-            title_found=0;
-            while(strlen(same_title_found[cs_count])!=0)
-        {
-            printf("%hi. %s\n",(short)(title_found+1),same_title_found[title_found]);
-            title_found++;
-            if(title_found==MAX_SAME_TITLE)
-            break; //Stop on reaching MAX_SAME_TITLE books of same title
-        }
-        */
-        for(int i=0;i<title_found;i++)
+         for(int i=0;i<title_found;i++)
             printf("%d. %s",i+1,same_title_found[i]);
-
+        printf("\nThere are %hi copies of %s available\n",title_found,title_search);
         }
 
          char ch;
@@ -1580,14 +1575,15 @@ void searchBookbyAuthor(BOOKNODE *head){
                 //If the search matches exactly, store the details
         else{
                 //printf("\n Found");
-                strcpy(auth_found,current->book.b_book_title);
-                strcat(all_same_auth[same_auth_count],"\nTitle: ");
+                strcpy(auth_found,current->book.b_book_author);
+                strcpy(all_same_auth[same_auth_count],"Title: ");
                 strcat(all_same_auth[same_auth_count],current->book.b_book_title);
                 char curr_ID[11];
                 sprintf(curr_ID, "%ld", current->book.b_book_ID); //Appending ID to display
                 strcat(all_same_auth[same_auth_count],"\n\tID: ");
                 strcat(all_same_auth[same_auth_count],curr_ID);
-
+                strcat(all_same_auth[same_auth_count],"\n\tAuthor: ");
+                 strcat(all_same_auth[same_auth_count],current->book.b_book_author);
                  strcat(all_same_auth[same_auth_count],"\n\tStatus: ");
                 if(current->book.b_book_status=='A')
                     strcat(all_same_auth[same_auth_count], "Available\n\n");
